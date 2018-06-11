e6f3150654d696fba56105e98989e4887135c659
kawaoka <br2jn51@yahoo.co.jp>
2018-06-11 19:30:38 +0900

    fix matcher bugfix

--- lib/rails_html_validator/matcher.rb
+++ lib/rails_html_validator/matcher.rb
@@ -2,11 +2,19 @@ require "nokogiri"
 
 module RailsHtmlValidator
   module Matchers
-    ::RSpec::Matchers.define :exclude_html_tag_of do |value, tag:|
+    ::RSpec::Matchers.define :exclude_html_tag_of do |attribute, tag:|
       match do |model|
         html = "<#{tag.to_s}></#{tag.to_s}>"
-        model.try("#{value}=", html)
-        !model.valid?
+        model.try("#{attribute}=", html)
+
+        validators = model._validators[attribute].select do |v|
+          v.class == ActiveModel::Validations::HtmlValidator
+        end
+        return false if validators.blank?
+
+        validators.first.validate_each(model, attribute, html)
+        p model.errors.messages[attribute]
+        model.errors.messages[attribute].length > 0
       end
     end
   end
--- lib/rails_html_validator/version.rb
+++ lib/rails_html_validator/version.rb
@@ -1,3 +1,3 @@
 module RailsHtmlValidator
-  VERSION = "0.1.6"
+  VERSION = "0.1.7"
 end
